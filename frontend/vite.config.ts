4ea0d8dc4b2b64ffd10402308cfea63fc0bdb17e
Bichev <vl.bichev@gmail.com>
2025-06-23 17:35:04 -0400

    aws config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3005,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://localhost:3002',
